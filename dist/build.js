46999bc3ae3e6e75a2d31466610387a7ef993c0e
Daniel Yang <miniflycn@justany.net>
2014-10-27 17:39:06 +0800

    tab 2 space

--- dist/build.js
+++ dist/build.js
@@ -41,31 +41,16 @@
 
     function catTimeout(foo) {
         return function(cb, timeout) {
-
             // for setTimeout(string, delay)
-
             if (typeof cb === 'string') {
-
-
                 try {
-
-
-
                     cb = new Function(cb);
-
-
                 } catch (e) {
-
-
-
                     _onthrow(e);
-
-
                 }
-
             }
-
             var args = [].slice.apply(arguments, 2);
+            // for setTimeout(function, delay, param1, ...)
             cb = cat(cb, args.length && args);
             foo(cb, timeout);
         }
--- src/core/timeout.js
+++ src/core/timeout.js
@@ -1,15 +1,15 @@
 function catTimeout(foo) {
     return function (cb, timeout) {
-    	// for setTimeout(string, delay)
-    	if (typeof cb === 'string') {
-    		try {
-    			cb = new Function(cb);
-    		} catch(e) {
-    			_onthrow(e);
-    		}
-    	}
-    	var args = [].slice.apply(arguments, 2);
-    	// for setTimeout(function, delay, param1, ...)
+        // for setTimeout(string, delay)
+        if (typeof cb === 'string') {
+            try {
+                cb = new Function(cb);
+            } catch(e) {
+                _onthrow(e);
+            }
+        }
+        var args = [].slice.apply(arguments, 2);
+        // for setTimeout(function, delay, param1, ...)
         cb = cat(cb, args.length && args);
         foo(cb, timeout);
     }
