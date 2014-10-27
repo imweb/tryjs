5c83f02dd21cd74cb1313f18ed2d715415be8ab9
yorts52 <yorts52@qq.com>
2014-10-27 18:32:05 +0800

    xx

--- dist/build.js
+++ dist/build.js
@@ -56,12 +56,12 @@
             var args = [].slice.call(arguments, 2);
             // for setTimeout(function, delay, param1, ...)
             cb = cat(cb, args.length && args);
-            foo(cb, timeout);
+            return foo(cb, timeout);
         }
     }
 
     root.setTimeout = catTimeout(root.setTimeout);
-    root.setIntveral = catTimeout(root.setInterval);
+    root.setInterval = catTimeout(root.setInterval);
 
     var _require = root.require,
         _define = root.define;
