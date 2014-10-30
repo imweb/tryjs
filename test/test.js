9ff88acc801de0c951dcc021da3aab95cf5ad1f5
Daniel Yang <miniflycn@justany.net>
2014-10-30 20:01:22 +0800

    test case ignore IE10-

--- test/test.js
+++ test/test.js
@@ -19,66 +19,77 @@ describe('tryjs', function () {
         Callback.done(e);
     };
 
-    it('should catch require error', function (done) {
-        Callback.then(function (e) {
-            expect(e).to.match(/asf/);
-            done();
-        });
-        define('./test1', 'test');
-        require(['./test1'], function (test) {
-            // undefined
-            console.log(asf);
-        });
-    });
+    var modern = true;
+    try {
+        badjsIngore();
+    } catch(e) {
+        if (!e.stack) modern = false;
+    }
 
-    it('should catch define error', function (done) {
-        Callback.then(function (e) {
-            expect(e).to.match(/fgh/);
-            done();
+    // for modern bowser
+    if (modern) {
+
+        it('should catch require error', function (done) {
+            Callback.then(function (e) {
+                expect(e).to.match(/asf/);
+                done();
+            });
+            define('./test1', 'test');
+            require(['./test1'], function (test) {
+                // undefined
+                console.log(asf);
+            });
         });
-        define('./test2', function (test) {
-            console.log(fgh);
+
+        it('should catch define error', function (done) {
+            Callback.then(function (e) {
+                expect(e).to.match(/fgh/);
+                done();
+            });
+            define('./test2', function (test) {
+                console.log(fgh);
+            });
+            require(['./test2']);
         });
-        require(['./test2']);
-    });
 
-    it('should catch setTimeout error', function (done) {
-        Callback.then(function (e) {
-            expect(e).to.match(/xx/);
-            done();
+        it('should catch setTimeout error', function (done) {
+            Callback.then(function (e) {
+                expect(e).to.match(/xx/);
+                done();
+            });
+            setTimeout(function(){
+                alert(xx);
+            }, 0);
         });
-        setTimeout(function(){
-            alert(xx);
-        }, 0);
-    });
 
-    it('should catch setInterval error', function (done) {
-        Callback.then(function (e) {
-            expect(e).to.match(/oo/);
-            done();
+        it('should catch setInterval error', function (done) {
+            Callback.then(function (e) {
+                expect(e).to.match(/oo/);
+                done();
+            });
+            var timer = setInterval(function(){
+                clearInterval(timer);
+                alert(oo);
+            }, 0);
         });
-        var timer = setInterval(function(){
-            clearInterval(timer);
-            alert(oo);
-        }, 0);
-    });
 
-    it('should catch event error', function (done) {
-        Callback.then(function (e) {
-            expect(e).to.match(/ttt/);
-            done();
+        it('should catch event error', function (done) {
+            Callback.then(function (e) {
+                expect(e).to.match(/ttt/);
+                done();
+            });
+            $('<a href="javascript:void(0);" id="test"></a>').appendTo(document.body)
+                .on('click', function (e) {
+                    alert(ttt);
+                }).click();
         });
-        $('<a href="javascript:void(0);" id="test"></a>').appendTo(document.body)
-            .on('click', function (e) {
-                alert(ttt);
-            }).click();
-    });
 
 
 
-    it('if method of require or define attached attribute', function () {
-        expect(define.amd.jQuery).to.equal(true);
-        expect(require.testA).to.equal('testA');
-        expect(define.testB).to.equal('testB');
-    });
+        it('if method of require or define attached attribute', function () {
+            expect(define.amd.jQuery).to.equal(true);
+            expect(require.testA).to.equal('testA');
+            expect(define.testB).to.equal('testB');
+        });
+    }
 });
